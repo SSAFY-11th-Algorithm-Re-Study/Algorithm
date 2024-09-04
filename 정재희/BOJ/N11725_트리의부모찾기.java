b44e83a217ad72627ccd33498cd2e8eda6a1fcbf
jaehee Jung <wjfwogml5890@naver.com>
2024-09-05 00:18:11 +0900

    [BOJ] N11725 트리의 부모찾기(sysout 수정) 65,752KB | 492ms

--- 정재희/BOJ/N11725_트리의부모찾기.java
+++ 정재희/BOJ/N11725_트리의부모찾기.java
@@ -5,48 +5,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class N11725_트리의부모찾기 {
 
 	/**
-	 * 
-	 * 	72,984KB | 1,348ms
+	 * 65,752KB	| 492ms
 	 */
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		
 		List<Integer>[] tree = new ArrayList[N+1];
-		
+		for (int i = 0; i <= N; i++) {
+			tree[i] = new ArrayList<Integer>();
+		}
 		for (int i = 0; i < N-1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			if(tree[a] == null) tree[a] = new ArrayList<Integer>();
-			if(tree[b] == null) tree[b] = new ArrayList<Integer>();
+
 			tree[a].add(b);
 			tree[b].add(a);
 		}
 		
-		Queue<Integer> queue = new ArrayDeque<Integer>();
+		Deque<Integer> queue = new ArrayDeque<Integer>();
 		queue.offer(1);
 		int[] parents = new int[N+1];
-		while(!queue.isEmpty()) {
+		int counts = 1;
+		while(!queue.isEmpty() || counts < N) {
 			int par = queue.poll();
 			for (int child: tree[par]) {
 				if(parents[child] != 0) continue;
 				parents[child] = par;
 				queue.offer(child);
+				counts++;
 			}
 		}
 		
 		for (int i = 2; i <=N; i++) {
-			System.out.println(parents[i]);
+			sb.append(parents[i]).append("\n");
 		}
+		System.out.println(sb);
 	}
 
 }
